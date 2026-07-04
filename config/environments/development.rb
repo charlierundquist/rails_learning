46b14784351f2fcb0217e8be9c77dd0e895249ec
Charlie Rundquist <charlierundquist@gmail.com>
2026-07-04 08:55:40 -0500

    trying to add credentials

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,13 +32,29 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+
+  # Set host to be used by links generated in mailer templates.
+  config.action_mailer.default_url_options = { host: "testing.charlierundquist.com" }
+
+  # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: Rails.application.credentials.dig(:smtp, :user_name),
+    password: Rails.application.credentials.dig(:smtp, :password),
+    address: "smtp.protonmail.ch",
+    domain: "testing.charlierundquist.com",
+    port: 587,
+    authentication: :plain,
+    enable_starttls: true
+  }
 
   # Make template changes take effect immediately.
   config.action_mailer.perform_caching = false
 
   # Set localhost to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  # config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
