e2a0325c2bbaa988e5b125455cd9c66a2387fbb0
Charlie Rundquist <charlierundquist@gmail.com>
2026-07-05 16:14:48 -0500

    changed email domain

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: email_address_with_name("charlie@dawnskywebdesign.com", "Rails Learning")
+  default from: email_address_with_name("charlie@charlierundquist.com", "Rails Learning")
   layout "mailer"
 end
